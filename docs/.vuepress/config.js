773ea32d5c90c532bef9c498faf8d4b5184efa27
jet6666 <9you.com@gmail.com>
2021-03-11 16:44:09 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: 'XXX Docs',
+  description: 'XX doc description',
+themeConfig: {
+    logo: '/logo.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+  sidebar:  [
+  ['/', 'Android Configureation'],
+   ['/web', 'WEB Configureation'],
+   ['/ios', 'IOS Configureation']
+
+
+  ]
+  }
+
+}
+ 
---
+++ docs/README.md
@@ -0,0 +1,23 @@
+
+# android
+
+
+## Android Configureation
+
+12323424
+
+### Configureation
+
+123131231
+
+### Configureation 2
+
+1231231321
+
+
+## Configureation
+
+3123313132
+
+
+
